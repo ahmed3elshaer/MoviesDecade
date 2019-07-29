7510fe03b8eca155e9eb3e737f7f1da63f28c373
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-07-29 13:22:51 +0200

    Creating ViewStates for Movies View

--- app/src/main/java/com/ahmed3elshaer/moviesdecade/movies/MoviesViewStates.kt
+++ app/src/main/java/com/ahmed3elshaer/moviesdecade/movies/MoviesViewStates.kt
@@ -1,8 +1,21 @@
 package com.ahmed3elshaer.moviesdecade.movies
 
-import com.ahmed3elshaer.moviesdecade.mvibase.MviResult
+import com.ahmed3elshaer.moviesdecade.data.models.Movie
 import com.ahmed3elshaer.moviesdecade.mvibase.MviViewState
 
-sealed class MoviesViewStates :MviViewState {
+data class MoviesViewStates(
+    val isLoading: Boolean,
+    val movies: List<Movie>,
+    val error: Throwable?
+) : MviViewState {
+    companion object {
+        fun idle(): MoviesViewStates {
+            return MoviesViewStates(
+                isLoading = false,
+                movies = listOf(),
+                error = null
+            )
+        }
+    }
 
 }
