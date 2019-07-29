3a39cb5f7846a8fa5906588180619958a2f32320
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-07-29 05:31:03 +0200

    stating actions for Movies

---
+++ app/src/main/java/com/ahmed3elshaer/moviesdecade/movies/MoviesActions.kt
@@ -0,0 +1,8 @@
+package com.ahmed3elshaer.moviesdecade.movies
+
+import com.ahmed3elshaer.moviesdecade.mvibase.MviAction
+
+sealed class MoviesActions :MviAction {
+    object LoadMovies: MoviesActions()
+    data class SearchMovies(val query:String): MoviesActions()
+}
