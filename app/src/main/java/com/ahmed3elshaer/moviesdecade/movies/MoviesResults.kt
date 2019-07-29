d290b615641214aea54328bdc6784b7b6f8c7320
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-07-29 13:21:27 +0200

    Creating LoadMovies result states

--- app/src/main/java/com/ahmed3elshaer/moviesdecade/movies/MoviesResults.kt
+++ app/src/main/java/com/ahmed3elshaer/moviesdecade/movies/MoviesResults.kt
@@ -1,7 +1,12 @@
 package com.ahmed3elshaer.moviesdecade.movies
 
+import com.ahmed3elshaer.moviesdecade.data.models.Movie
 import com.ahmed3elshaer.moviesdecade.mvibase.MviResult
 
-sealed class MoviesResults :MviResult {
-
+sealed class MoviesResults : MviResult {
+    sealed class LoadMoviesResult : MoviesResults() {
+        data class Success(val movies: List<Movie>) : LoadMoviesResult()
+        data class Failure(val error: Throwable) : LoadMoviesResult()
+        object InFlight : LoadMoviesResult()
+    }
 }
