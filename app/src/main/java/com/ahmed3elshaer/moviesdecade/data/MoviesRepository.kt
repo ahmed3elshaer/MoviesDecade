ef6fdf3a9895ccccab16a9bfde960381f3fc889a
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-07-29 13:13:30 +0200

    providing context to repository

--- app/src/main/java/com/ahmed3elshaer/moviesdecade/data/MoviesRepository.kt
+++ app/src/main/java/com/ahmed3elshaer/moviesdecade/data/MoviesRepository.kt
@@ -1,12 +1,17 @@
 package com.ahmed3elshaer.moviesdecade.data
 
+import android.content.Context
 import com.ahmed3elshaer.moviesdecade.data.models.Movie
+import com.ahmed3elshaer.moviesdecade.data.models.MoviesResponse
 import com.ahmed3elshaer.moviesdecade.network.FlickerApi
+import com.ahmed3elshaer.moviesdecade.utils.readAssetsFile
+import com.squareup.moshi.Moshi
 import io.reactivex.Observable
+import java.util.*
 import javax.inject.Inject
 
-class MoviesRepository @Inject constructor(flickerApi: FlickerApi) {
 
+class MoviesRepository @Inject constructor(var flickerApi: FlickerApi, var context: Context) {
     fun getMovies(): Observable<List<Movie>> {
         return Observable.just(mutableListOf())
     }
