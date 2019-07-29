0598f24088057b08ee5a5d45b651198814083e0d
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-07-29 13:20:16 +0200

    Implementing processors for LoadMovies and SearchMovies Intents

--- app/src/main/java/com/ahmed3elshaer/moviesdecade/movies/MoviesActionProcessor.kt
+++ app/src/main/java/com/ahmed3elshaer/moviesdecade/movies/MoviesActionProcessor.kt
@@ -4,35 +4,49 @@ import com.ahmed3elshaer.moviesdecade.data.MoviesRepository
 import com.ahmed3elshaer.moviesdecade.utils.schedulers.BaseSchedulerProvider
 import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
+import java.lang.Thread.sleep
+import java.util.logging.Handler
 
 class MoviesActionProcessor(moviesRepo: MoviesRepository, scheduler: BaseSchedulerProvider) {
 
-    var actionProcessor: ObservableTransformer<MoviesActions, MoviesResults> =
-        ObservableTransformer { action ->
-            action.publish { shared ->
+    var actionProcessor =
+        ObservableTransformer<MoviesActions,MoviesResults> { actions ->
+            actions.publish { shared ->
                 Observable.merge(
                     shared.ofType(MoviesActions.LoadMovies::class.java).compose(loadMoviesProcessor),
-                    shared.ofType(MoviesActions.SearchMovies::class.java).compose(
-                        searchMoviesProcessor
-                    )
+                    shared.ofType(MoviesActions.SearchMovies::class.java).compose(searchMoviesProcessor)
                 )
             }
         }
 
-    val loadMoviesProcessor =
-        ObservableTransformer { action: Observable<MoviesActions.LoadMovies> ->
-            action.flatMap { loadAction ->
-                //TODO get movies from repo
+    private val loadMoviesProcessor =
+        ObservableTransformer<MoviesActions.LoadMovies,MoviesResults> { action ->
+            action.flatMap {
                 moviesRepo.getMovies()
-                    .map { movies ->  }
+                    .map { movies ->
+                        MoviesResults.LoadMoviesResult.Success(movies)
+                    }
+                    .cast(MoviesResults.LoadMoviesResult::class.java)
+                    .onErrorReturn (MoviesResults.LoadMoviesResult::Failure)
+                    .subscribeOn(scheduler.io())
+                    .observeOn(scheduler.ui())
+                    .startWith(MoviesResults.LoadMoviesResult.InFlight)
             }
 
         }
 
-    val loadMoviesProcessor =
-        ObservableTransformer { action: Observable<MoviesActions.LoadMovies> ->
-            action.flatMap { loadAction ->
-                //TODO get movies from repo
+    private val searchMoviesProcessor =
+        ObservableTransformer<MoviesActions.SearchMovies,MoviesResults> { action ->
+            action.flatMap { searchAction ->
+                moviesRepo.searchMovies(searchAction.query)
+                    .map { movies ->
+                        MoviesResults.LoadMoviesResult.Success(movies)
+                    }
+                    .cast(MoviesResults.LoadMoviesResult::class.java)
+                    .onErrorReturn (MoviesResults.LoadMoviesResult::Failure)
+                    .subscribeOn(scheduler.io())
+                    .observeOn(scheduler.ui())
+                    .startWith(MoviesResults.LoadMoviesResult.InFlight)
 
             }
 
