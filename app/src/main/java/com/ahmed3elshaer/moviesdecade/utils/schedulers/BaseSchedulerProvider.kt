949cf4fae4b720b5849f0044f32479de497c4a09
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-07-29 05:35:46 +0200

    Singleton schedulers helpers

---
+++ app/src/main/java/com/ahmed3elshaer/moviesdecade/utils/schedulers/BaseSchedulerProvider.kt
@@ -0,0 +1,14 @@
+package com.ahmed3elshaer.moviesdecade.utils.schedulers
+
+import io.reactivex.Scheduler
+
+/**
+ * Allow providing different types of [Scheduler]s.
+ */
+interface BaseSchedulerProvider {
+  fun computation(): Scheduler
+
+  fun io(): Scheduler
+
+  fun ui(): Scheduler
+}
---
+++ app/src/main/java/com/ahmed3elshaer/moviesdecade/utils/schedulers/SchedulerProvider.kt
@@ -0,0 +1,16 @@
+package com.ahmed3elshaer.moviesdecade.utils.schedulers
+
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * Provides different types of schedulers.
+ */
+object SchedulerProvider : BaseSchedulerProvider {
+  override fun computation(): Scheduler = Schedulers.computation()
+
+  override fun io(): Scheduler = Schedulers.io()
+
+  override fun ui(): Scheduler = AndroidSchedulers.mainThread()
+}
