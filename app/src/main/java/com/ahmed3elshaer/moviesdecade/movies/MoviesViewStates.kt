f5abd60ed883e25c75635af0058fc6859ac7df4d
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-07-29 05:33:04 +0200

    Preparing ViewState for Movies

---
+++ app/src/main/java/com/ahmed3elshaer/moviesdecade/movies/MoviesViewStates.kt
@@ -0,0 +1,8 @@
+package com.ahmed3elshaer.moviesdecade.movies
+
+import com.ahmed3elshaer.moviesdecade.mvibase.MviResult
+import com.ahmed3elshaer.moviesdecade.mvibase.MviViewState
+
+sealed class MoviesViewStates :MviViewState {
+
+}
