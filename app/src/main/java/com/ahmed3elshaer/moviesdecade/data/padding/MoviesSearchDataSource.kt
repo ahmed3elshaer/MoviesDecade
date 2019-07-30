7e6dc24f7d544ce56c8d08f58b9372944789bf60
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-07-30 03:29:26 +0200

    Datasource for search pagination

--- app/src/main/java/com/ahmed3elshaer/moviesdecade/data/padding/MoviesSearchDataSource.kt
+++ app/src/main/java/com/ahmed3elshaer/moviesdecade/data/padding/MoviesSearchDataSource.kt
@@ -1,27 +1,33 @@
 package com.ahmed3elshaer.moviesdecade.data.padding
 
+import android.util.Log
 import androidx.paging.PageKeyedDataSource
 import com.ahmed3elshaer.moviesdecade.data.MoviesRepository
 import com.ahmed3elshaer.moviesdecade.data.models.Movie
 
 class MoviesSearchDataSource(
-    var moviesRepository: MoviesRepository
+    var moviesRepository: MoviesRepository,
+    var query: String
 ) :
-    PageKeyedDataSource<Int, Movie>() {
+    PageKeyedDataSource<Int, Any>() {
+
     override fun loadInitial(
         params: LoadInitialParams<Int>,
-        callback: LoadInitialCallback<Int, Movie>
+        callback: LoadInitialCallback<Int, Any>
     ) {
-        callback.onResult(moviesRepository.getMovies(0), null, 1)
+        callback.onResult(moviesRepository.searchMovies(query), null, 1)
 
     }
 
-    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Movie>) {
-        callback.onResult(moviesRepository.getMovies(params.key), params.key + 1)
+    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Any>) {
+        val adjacentKey = if (moviesRepository.currentSearchPage() <= params.key)
+            null
+        else
+            params.key + 1
+        callback.onResult(moviesRepository.loadMoreSearch(params.key), adjacentKey)
     }
 
-    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Movie>) {
-        callback.onResult(moviesRepository.getMovies(params.key), params.key - 1)
+    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Any>) {
     }
 
 }
