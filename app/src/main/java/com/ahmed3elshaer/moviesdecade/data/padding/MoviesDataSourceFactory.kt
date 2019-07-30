f70f1fe937a86aec3f7571afadf94bf74e36835e
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-07-30 03:27:56 +0200

    Creating Factory for injecting into datasource

--- app/src/main/java/com/ahmed3elshaer/moviesdecade/data/padding/MoviesDataSourceFactory.kt
+++ app/src/main/java/com/ahmed3elshaer/moviesdecade/data/padding/MoviesDataSourceFactory.kt
@@ -1,30 +1,17 @@
 package com.ahmed3elshaer.moviesdecade.data.padding
 
-import android.content.Context
-import androidx.paging.PageKeyedDataSource
+import androidx.paging.DataSource
 import com.ahmed3elshaer.moviesdecade.data.MoviesRepository
 import com.ahmed3elshaer.moviesdecade.data.models.Movie
-import io.reactivex.Scheduler
-import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class MoviesDataSourceFactory @Inject constructor(
     var moviesRepository: MoviesRepository
 ) :
-    PageKeyedDataSource<Int, Movie>() {
-    override fun loadInitial(
-        params: LoadInitialParams<Int>,
-        callback: LoadInitialCallback<Int, Movie>
-    ) {
-        moviesRepository.getMovies(0)
-        callback.onResult(moviesRepository.getMovies(params.requestedLoadSize), null, 1)
-
+    DataSource.Factory<Int, Movie>() {
+    override fun create(): DataSource<Int, Movie> {
+        return MoviesDataSource(moviesRepository)
     }
 
-    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Movie>) {
-    }
-
-    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Movie>) {
-    }
 
 }
