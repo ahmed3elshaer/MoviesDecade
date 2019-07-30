eff71db6be62fff8b9d418d3773b8dff5c8a649d
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-07-30 15:16:42 +0200

    adding Flicker keys

--- app/src/main/java/com/ahmed3elshaer/moviesdecade/utils/Constants.kt
+++ app/src/main/java/com/ahmed3elshaer/moviesdecade/utils/Constants.kt
@@ -1,7 +1,11 @@
 package com.ahmed3elshaer.moviesdecade.utils
 
-const val BASE_URL = "https://www.google.com/"
+const val BASE_URL = "https://api.flickr.com/"
 const val TYPE_YEAR: Int=22
 const val TYPE_MOVIE: Int=33
 const val PAGE_COUNT = 12
 const val PAGE_COUNT_MOVIES = 10
+const val MOVIE_KEY = "movieModel"
+const val MOVIE_DETAIL = "movieDetail"
+const val FLICKER_API_KEY = "b2b66e236bf6e44ddac4ee41e13f6b32"
+const val FLICKER_SECRET = "e9f08376a91197aa"
