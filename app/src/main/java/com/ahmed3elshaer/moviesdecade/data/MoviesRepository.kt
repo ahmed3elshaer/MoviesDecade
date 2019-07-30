94c51f11e4e4911e1c8f71def7cb1040cb2c9108
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-07-30 03:27:00 +0200

    refactor repository to handle data manipulation on MoviesProvider

--- app/src/main/java/com/ahmed3elshaer/moviesdecade/data/MoviesRepository.kt
+++ app/src/main/java/com/ahmed3elshaer/moviesdecade/data/MoviesRepository.kt
@@ -2,41 +2,34 @@ package com.ahmed3elshaer.moviesdecade.data
 
 import android.content.Context
 import com.ahmed3elshaer.moviesdecade.data.models.Movie
-import com.ahmed3elshaer.moviesdecade.data.models.MoviesResponse
 import com.ahmed3elshaer.moviesdecade.network.FlickerApi
-import com.ahmed3elshaer.moviesdecade.utils.readAssetsFile
-import com.squareup.moshi.Moshi
-import io.reactivex.Observable
-import java.util.*
 import javax.inject.Inject
 
 
-class MoviesRepository @Inject constructor(var flickerApi: FlickerApi, var context: Context) {
-    private var moviesList = listOf<Movie>()
-    fun getMovies(): Observable<List<Movie>> {
-        return if (moviesList.isNotEmpty()) {
-            Observable.just(moviesList)
-        } else {
-            val moviesStr = context.assets.readAssetsFile("movies.json")
-            val moshi = Moshi.Builder().build()
-            val jsonAdapter = moshi.adapter(MoviesResponse::class.java)
-            moviesList = jsonAdapter.fromJson(moviesStr).movies
-            Observable.just(moviesList)
-        }
+class MoviesRepository @Inject constructor(
+    var flickerApi: FlickerApi,
+    var context: Context
+) {
 
+    fun getMovies(page: Int): List<Movie> {
+        return MoviesProvider.getMovies(context, page)
     }
 
-    fun searchMovies(query: String): Observable<List<Movie>> {
-        if (query.isEmpty())
-            return Observable.just(moviesList)
-        return if (moviesList.isNotEmpty()) {
-            Observable.just(moviesList.filter {
-                it.title.toLowerCase(Locale.getDefault())
-                    .contains(query.toLowerCase(Locale.getDefault()))
-            })
-        } else
-            Observable.error(Throwable("Empty movies file"))
 
+    fun searchMovies(query: String): List<Any> {
+        return MoviesProvider.searchMovies(context, query)
     }
+
+    fun loadMoreSearch(page: Int): List<Any> {
+        return MoviesProvider.loadMoreSearch(page)
+    }
+
+    fun currentSearchPage():Int{
+        return MoviesProvider.getCurrentSearchSize()
+    }
+    fun currentMoviesPage():Int{
+        return MoviesProvider.getCurrentMoviesSize()
+    }
+
 
 }
