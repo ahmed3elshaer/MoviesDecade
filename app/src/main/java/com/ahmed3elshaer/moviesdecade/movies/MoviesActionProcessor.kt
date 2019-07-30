40f729b14bf8ddecc407ba91fe03d50c72e384d1
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-07-30 03:33:03 +0200

    using pagination datasource instead of direct repository for pagination

--- app/src/main/java/com/ahmed3elshaer/moviesdecade/movies/MoviesActionProcessor.kt
+++ app/src/main/java/com/ahmed3elshaer/moviesdecade/movies/MoviesActionProcessor.kt
@@ -1,33 +1,55 @@
 package com.ahmed3elshaer.moviesdecade.movies
 
+import androidx.paging.PagedList
+import androidx.paging.PagedList.Config.MAX_SIZE_UNBOUNDED
+import androidx.paging.RxPagedListBuilder
 import com.ahmed3elshaer.moviesdecade.data.MoviesRepository
+import com.ahmed3elshaer.moviesdecade.data.models.Movie
+import com.ahmed3elshaer.moviesdecade.data.padding.MoviesDataSource
+import com.ahmed3elshaer.moviesdecade.data.padding.MoviesDataSourceFactory
+import com.ahmed3elshaer.moviesdecade.data.padding.MoviesSearchDataSourceFactory
+import com.ahmed3elshaer.moviesdecade.utils.PAGE_COUNT
+import com.ahmed3elshaer.moviesdecade.utils.PAGE_COUNT_MOVIES
 import com.ahmed3elshaer.moviesdecade.utils.schedulers.BaseSchedulerProvider
 import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
-import java.lang.Thread.sleep
-import java.util.logging.Handler
 
-class MoviesActionProcessor(moviesRepo: MoviesRepository, scheduler: BaseSchedulerProvider) {
+@Suppress("UNCHECKED_CAST")
+class MoviesActionProcessor(
+    moviesRepo: MoviesRepository,
+    moviesDataSourceFactory: MoviesDataSourceFactory,
+    scheduler: BaseSchedulerProvider
+) {
 
     var actionProcessor =
-        ObservableTransformer<MoviesActions,MoviesResults> { actions ->
+        ObservableTransformer<MoviesActions, MoviesResults> { actions ->
             actions.publish { shared ->
                 Observable.merge(
                     shared.ofType(MoviesActions.LoadMovies::class.java).compose(loadMoviesProcessor),
-                    shared.ofType(MoviesActions.SearchMovies::class.java).compose(searchMoviesProcessor)
+                    shared.ofType(MoviesActions.SearchMovies::class.java).compose(
+                        searchMoviesProcessor
+                    )
                 )
             }
         }
 
     private val loadMoviesProcessor =
-        ObservableTransformer<MoviesActions.LoadMovies,MoviesResults> { action ->
+        ObservableTransformer<MoviesActions.LoadMovies, MoviesResults> { action ->
             action.flatMap {
-                moviesRepo.getMovies()
+                val config = PagedList.Config.Builder()
+                    .setPageSize(PAGE_COUNT_MOVIES)
+                    .setInitialLoadSizeHint(PAGE_COUNT_MOVIES)
+                    .setEnablePlaceholders(false)
+                    .setInitialLoadSizeHint(0)
+                    .setMaxSize(MAX_SIZE_UNBOUNDED)
+                    .build()
+                RxPagedListBuilder(moviesDataSourceFactory, config)
+                    .buildObservable()
                     .map { movies ->
-                        MoviesResults.LoadMoviesResult.Success(movies)
+                        MoviesResults.LoadMoviesResult.Success(movies as PagedList<Any>)
                     }
                     .cast(MoviesResults.LoadMoviesResult::class.java)
-                    .onErrorReturn (MoviesResults.LoadMoviesResult::Failure)
+                    .onErrorReturn(MoviesResults.LoadMoviesResult::Failure)
                     .subscribeOn(scheduler.io())
                     .observeOn(scheduler.ui())
                     .startWith(MoviesResults.LoadMoviesResult.InFlight)
