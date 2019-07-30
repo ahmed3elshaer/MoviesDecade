e935857dfcfbe6552ae83c1949a2d6dc2f68ec85
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-07-30 08:34:27 +0200

    altering movies pagination count

--- app/src/main/java/com/ahmed3elshaer/moviesdecade/utils/Constants.kt
+++ app/src/main/java/com/ahmed3elshaer/moviesdecade/utils/Constants.kt
@@ -4,4 +4,4 @@ const val BASE_URL = "https://www.google.com/"
 const val TYPE_YEAR: Int=22
 const val TYPE_MOVIE: Int=33
 const val PAGE_COUNT = 12
-const val PAGE_COUNT_MOVIES = 4
+const val PAGE_COUNT_MOVIES = 10
