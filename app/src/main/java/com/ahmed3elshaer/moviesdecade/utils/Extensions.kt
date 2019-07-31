b6f8c53ce54ef5512b54bc6677cb5b1989f5cdcf
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2019-07-31 17:34:24 +0200

    adding mock PagedList for test

--- app/src/main/java/com/ahmed3elshaer/moviesdecade/utils/Extensions.kt
+++ app/src/main/java/com/ahmed3elshaer/moviesdecade/utils/Extensions.kt
@@ -2,10 +2,13 @@ package com.ahmed3elshaer.moviesdecade.utils
 
 import android.content.res.AssetManager
 import android.view.View
+import androidx.paging.PagedList
 import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.annotations.CheckReturnValue
 import io.reactivex.annotations.SchedulerSupport
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito
 import java.util.concurrent.Executors
 
 
@@ -24,21 +27,33 @@ fun <T : Any, U : Any> Observable<T>.notOfType(clazz: Class<U>): Observable<T> {
     return filter { !clazz.isInstance(it) }
 }
 
-fun View.hide(){
+fun View.hide() {
     this.visibility = View.GONE
 }
 
 
-fun View.show(){
+fun View.show() {
     this.visibility = View.VISIBLE
 }
- fun View.invisible() {
-     this.visibility = View.INVISIBLE
+
+fun View.invisible() {
+    this.visibility = View.INVISIBLE
 }
 
 
-fun AssetManager.readAssetsFile(fileName : String): String = open(fileName).bufferedReader().use{it.readText()}
+fun AssetManager.readAssetsFile(fileName: String): String =
+    open(fileName).bufferedReader().use { it.readText() }
 
 fun ioThread(f: () -> Unit) {
     Executors.newSingleThreadExecutor().execute(f)
+}
+
+fun <T> List<T>.mockPagedList(): PagedList<T> {
+    val pagedList = Mockito.mock(PagedList::class.java) as PagedList<T>
+    Mockito.`when`(pagedList.get(ArgumentMatchers.anyInt())).then { invocation ->
+        val index = invocation.arguments.first() as Int
+        this[index]
+    }
+    Mockito.`when`(pagedList.size).thenReturn(this.size)
+    return pagedList
 }
